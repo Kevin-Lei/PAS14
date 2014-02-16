0a9ec2b757cf18689b4432791f6e5e053cd874bd
Kevin Lei <kevinlei@seas.upenn.edu>
2014-02-16 15:34:51 -0500

    nothing

---
+++ ebaysdk-python-master/build/lib/ebaysdk/exception.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''
+© 2012-2013 eBay Software Foundation
+Authored by: Tim Keefer
+Licensed under CDDL 1.0
+'''
+
+class ConnectionError(Exception):
+    pass
+
+class ConnectionResponseError(Exception):
+    pass
